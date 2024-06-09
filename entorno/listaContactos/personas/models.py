c59fe8c2aac64b8b5a8bfb7606aea538b85779e0
UJKMjuandi <jgutierrezcc@unsa.edu.pe>
2024-06-08 21:11:36 -0500

    editando la app persona\models.py con lo qye dice la diapositiva

--- entorno/listaContactos/personas/models.py
+++ entorno/listaContactos/personas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Persona(models.Model):
+    nombres = models.TextField()
+    apellidos = models.TextField()
+    edad = models.TextField()
+
+INSTALLED_APPS = {
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'personas'
+}
