736d47a9f695825fa9843a07702619e9a308f617
UJKMjuandi <jgutierrezcc@unsa.edu.pe>
2024-06-12 08:14:58 -0500

    (Parte 02) Cambiando los tipos de campos

--- entorno/listaContactos/personas/models.py
+++ entorno/listaContactos/personas/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Persona(models.Model):
-    nombres = models.TextField()
-    apellidos = models.TextField()
-    edad = models.TextField()
+    nombres = models.CharField(max_length= 100)
+    apellidos = models.CharField(max_length= 100)
+    edad = models.IntegerField(max_digits = 3)#(max_d)
 
 INSTALLED_APPS = {
     'django.contrib.admin',
@@ -13,5 +13,5 @@ INSTALLED_APPS = {
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'personas'
+    'personas',
 }
